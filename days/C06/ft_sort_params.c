4e9acce9c87097c5aa87ce1a3a2470d7d0d187b5
Anwaroxxx <anwar.azarzou.6969@gmail.com>
2025-07-20 18:33:43 +0100

    added new exercies

--- days/C06/ft_sort_params.c
+++ days/C06/ft_sort_params.c
@@ -1,7 +1,38 @@
-#include<unistd.h>
-int main(int argc, char *argv[])
+#include <unistd.h>
+
+int main(int argc, char **argv)
 {
-    int i = 1;
-    int j;
-    char temp;
+    int i, j, k;
+    char *temp;
+
+    i = 1;
+    while (i < argc - 1)
+    {
+        j = i + 1;
+        while (j < argc)
+        {
+            k = 0;
+            while (argv[i][k] && argv[j][k] && argv[i][k] == argv[j][k])
+                k++;
+            if (argv[i][k] > argv[j][k])
+                temp = argv[i];
+                argv[i] = argv[j];
+                argv[j] = temp;
+            j++;
+        }
+        i++;
+    }
+    i = 1;
+    while (i < argc)
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            write(1, &argv[i][j], 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i++;
+    }
+    return 0;
 }
